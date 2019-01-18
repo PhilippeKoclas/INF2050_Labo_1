6d8fa173e068f72b6807213214dd9b1d3de56963
Koclas <fc891055@C39433L.ens.uqam.ca>
2019-01-18 14:46:46 -0500

    Modifiication de Fizz et ajouter .gitignore

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,7 +2,7 @@
 public class FizzBuzz
 {
 	public static void main(String[] args) {
-		for (int i = 1; i <= 100; i++)
+		for (int i = 1; i <= 200; i++)
 		{
 			if (i % 3 == 0)
 				System.out.println("Fizz");
@@ -14,5 +14,4 @@ public class FizzBuzz
 				System.out.println(i);
 		}
 	}
-
 }
